5d6a045b9989faa6e3765533dab250c09f1cbf58
tinawang113 <yunting.wang@edu.sait.ca>
2020-05-21 20:25:12 -0600

    [Week 1] Done

--- src/solutions/Main.java
+++ src/solutions/Main.java
@@ -10,17 +10,23 @@ package solutions;
 public class Main {
 
 	/**
+	 * the questions from leetcode
+	 * url: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/
 	 * @param args
 	 */
 	static Questions questions = new Questions();
 	public static void main(String[] args) {
-		int [] testArray = {4,1,2,1,2};
+		int [] testArray = {-2,1,-3,4,-1,2,1,-5,4};
 		int [] testArray_moveZeroes = {4,0,2,0,1};
+		String [] testString = {"eat", "tea", "tan", "ate", "nat", "bat"};
 		//single Number
-		System.out.println("Output: " + questions.singleNumber(testArray));
+		questions.singleNumber(testArray);
 		questions.happyNumber(19);
 		questions.moveZeroes(testArray_moveZeroes);
-
+		questions.max_Subarray(testArray);
+		questions.maxProfit(testArray);
+		questions.groupAnagrams_1(testString);
+		questions.groupAnagrams_2(testString);
 	}
 
 }
--- src/solutions/Questions.java
+++ src/solutions/Questions.java
@@ -4,6 +4,9 @@
 package solutions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author YunTing Wang
@@ -74,4 +77,84 @@ public class Questions {
 	            }
 	        }
 	    }
+	    
+	    
+	    public int max_Subarray(int[] nums) {
+			 int tempSum = 0;
+			 int max = nums[0];
+			 //if(nums.length == 1) return nums[0];
+			 for(int i=0; i< nums.length; i++){
+			     tempSum = 0;
+			     for(int j=i; j< nums.length; j++){          
+			         tempSum += nums[j];
+			         max = Math.max(max, tempSum);
+			     }
+			 }
+			 return max;
+	    }
+	    	
+	    public int maxProfit(int[] prices) {
+	        int profit = 0;
+	        for(int i=1; i<prices.length; i++){
+	            int diff = prices[i]-prices[i-1];
+	            if(diff > 0){
+	                profit += diff;
+	            }
+	        }
+	        return profit;
+	    }
+
+	    
+        public List<List<String>> groupAnagrams_1(String[] strs) {
+            List<List<String>> outList = new ArrayList<List<String>>();
+            int tempIdx = 0;
+            if(strs.length == 0 && strs == null) return outList;
+            ArrayList<String> record  = new ArrayList<String>();
+            List<String> list = new ArrayList<String>();
+            /*solution 1 - without hashMap*/
+            for(String str:strs){
+                char [] cs = str.toCharArray();
+                Arrays.sort(cs);
+                String sortedString = new String(cs);
+                if(record.indexOf(sortedString) != -1){
+                    //seeking the position of array.
+                    //add to the same group in outList
+                    tempIdx = record.indexOf(sortedString);
+                    outList.get(tempIdx).add(str);
+                }else{
+                    record.add(sortedString);
+                    list = new ArrayList<String>();
+                    list.add(str);
+                    outList.add(list);
+                }
+            }
+            return outList;
+        }
+        
+        public List<List<String>> groupAnagrams_2(String[] strs){
+        	List<List<String>> outList = new ArrayList<List<String>>();
+        	
+            if(strs.length == 0 && strs == null) return outList;
+            
+            ArrayList<String> record  = new ArrayList<String>();
+            
+            /* solution -1*/
+            HashMap<String, Integer> recordMap  = new HashMap<String, Integer>(); 
+            for(String str: strs){
+                char[] c = str.toCharArray();
+                Arrays.sort(c);
+                String compareStr = new String(c);
+                if(recordMap.containsKey(compareStr)){ //existing in the recordMap, it's the same group.
+                    List<String> list = outList.get(recordMap.get(compareStr));
+    				list.add(str);
+                }else {
+    				List<String> list = new ArrayList<String>();
+    				list.add(str);
+    				recordMap.put(compareStr, outList.size());
+    				outList.add(list);
+    			}
+            }
+            return outList;
+        }
+	    
 }
